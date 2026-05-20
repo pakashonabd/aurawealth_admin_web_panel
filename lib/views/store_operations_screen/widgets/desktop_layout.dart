09c25fefb9079ec6bf54ab6d29f07bb8c3a0d692
salmanf.rahman <salmanfarid43@gmail.com>
2026-05-20 14:41:27 +0600

    fix: stack cards vertically in column with full width

--- lib/views/store_operations_screen/widgets/desktop_layout.dart
+++ lib/views/store_operations_screen/widgets/desktop_layout.dart
@@ -15,28 +15,19 @@ class DesktopLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return SingleChildScrollView(
       padding: const EdgeInsets.fromLTRB(24, 0, 24, 24),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: Column(
         children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: creditGramsPanel
-                  .animate()
-                  .fadeIn(delay: 100.ms, duration: 500.ms)
-                  .slideX(begin: -0.05, end: 0, duration: 500.ms),
-            ),
-          ),
-          const SizedBox(width: 20),
-          Expanded(
-            child: SingleChildScrollView(
-              child: redeemCodePanel
-                  .animate()
-                  .fadeIn(delay: 150.ms, duration: 500.ms)
-                  .slideX(begin: 0.05, end: 0, duration: 500.ms),
-            ),
-          ),
+          creditGramsPanel
+              .animate()
+              .fadeIn(delay: 100.ms, duration: 500.ms)
+              .slideY(begin: 0.05, end: 0, duration: 500.ms),
+          const SizedBox(height: 20),
+          redeemCodePanel
+              .animate()
+              .fadeIn(delay: 150.ms, duration: 500.ms)
+              .slideY(begin: 0.05, end: 0, duration: 500.ms),
         ],
       ),
     );
