1117f43a135446c7841677ff16dd7b6be0e73f43
salmanf.rahman <salmanfarid43@gmail.com>
2026-07-19 19:47:49 +0600

    fix: removed all elevation/shadow from delivery toggle

--- lib/views/redemption/redemption_screen.dart
+++ lib/views/redemption/redemption_screen.dart
@@ -1041,18 +1041,6 @@ class _RedemptionScreenState extends State<RedemptionScreen>
         decoration: BoxDecoration(
           color: const Color(0xFFE8E8E8),
           borderRadius: BorderRadius.circular(22),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withValues(alpha: 0.15),
-              blurRadius: 6,
-              offset: const Offset(0, 2),
-            ),
-            BoxShadow(
-              color: Colors.white.withValues(alpha: 0.8),
-              blurRadius: 2,
-              offset: const Offset(0, -1),
-            ),
-          ],
         ),
         padding: const EdgeInsets.all(3),
         child: Stack(
@@ -1067,13 +1055,6 @@ class _RedemptionScreenState extends State<RedemptionScreen>
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(19),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withValues(alpha: 0.15),
-                      blurRadius: 6,
-                      offset: const Offset(0, 2),
-                    ),
-                  ],
                 ),
               ),
             ),
